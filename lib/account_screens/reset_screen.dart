45a91902aabe3dcf63b24c5c6882dbb11aa22f38
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-09-04 12:32:50 +0500

    reset screen

--- lib/account_screens/reset_screen.dart
+++ lib/account_screens/reset_screen.dart
@@ -11,12 +11,72 @@ class ResetScreen extends StatefulWidget {
 class __ResetScreenState extends State<ResetScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.transparent,
       body: SafeArea(
-        child: Column(
-          children: [],
+          child: Column(children: [
+        const Center(
+          child: Text(
+            "Reset",
+            style: TextStyle(
+              fontFamily: "Lalezar",
+              fontSize: 25,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
         ),
-      ),
+        const Text(
+          "Password",
+          style: TextStyle(
+            fontFamily: "Lalezar",
+            fontSize: 25,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Add a new password",
+                  filled: true,
+                  fillColor: const Color.fromRGBO(248, 248, 248, 0.092),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        width: 3,
+                        // color: const Color(0xffF22355).withAlpha(98),
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: const Color(0xffF22355).withAlpha(98),
+                      ),
+                      borderRadius: BorderRadius.circular(20)))),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Confirm password",
+                  filled: true,
+                  fillColor: const Color.fromRGBO(248, 248, 248, 0.092),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        width: 3,
+                        // color: const Color(0xffF22355).withAlpha(98),
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: const Color(0xffF22355).withAlpha(98),
+                      ),
+                      borderRadius: BorderRadius.circular(20)))),
+        ),
+      ])),
     );
   }
 }
