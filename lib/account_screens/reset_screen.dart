5812210b25f0ec38b6890a1c4b844dab5e2ac045
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-09-05 09:50:16 +0500

    Reset Screen complete designed today by me and Haiqa

--- lib/account_screens/reset_screen.dart
+++ lib/account_screens/reset_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musify_app/account_screens/home_screen.dart';
 
 class ResetScreen extends StatefulWidget {
   // ignore: unused_element
@@ -15,34 +16,31 @@ class __ResetScreenState extends State<ResetScreen> {
       backgroundColor: Colors.transparent,
       body: SafeArea(
           child: Column(children: [
+        const SizedBox(
+          height: 80,
+        ),
         const Center(
           child: Text(
             "Reset",
-            style: TextStyle(
-              fontFamily: "Lalezar",
-              fontSize: 25,
-              fontWeight: FontWeight.normal,
-            ),
+            style: const TextStyle(fontFamily: 'Lalezar', fontSize: 36),
           ),
         ),
         const Text(
           "Password",
-          style: TextStyle(
-            fontFamily: "Lalezar",
-            fontSize: 25,
-            fontWeight: FontWeight.normal,
-          ),
+          style: const TextStyle(fontFamily: 'Lalezar', fontSize: 36),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
           child: TextFormField(
               decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 30),
                   hintText: "Add a new password",
                   filled: true,
                   fillColor: const Color.fromRGBO(248, 248, 248, 0.092),
                   enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
+                      borderSide: const BorderSide(
                         width: 3,
                         // color: const Color(0xffF22355).withAlpha(98),
                       )),
@@ -53,19 +51,18 @@ class __ResetScreenState extends State<ResetScreen> {
                       ),
                       borderRadius: BorderRadius.circular(20)))),
         ),
-        SizedBox(
-          height: 30,
-        ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
           child: TextFormField(
               decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 30),
                   hintText: "Confirm password",
                   filled: true,
                   fillColor: const Color.fromRGBO(248, 248, 248, 0.092),
                   enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
+                      borderSide: const BorderSide(
                         width: 3,
                         // color: const Color(0xffF22355).withAlpha(98),
                       )),
@@ -75,6 +72,30 @@ class __ResetScreenState extends State<ResetScreen> {
                         color: const Color(0xffF22355).withAlpha(98),
                       ),
                       borderRadius: BorderRadius.circular(20)))),
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          height: 10,
+        ),
+        Center(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+            child: Container(
+              height: 70,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 242, 35, 83),
+                  borderRadius: BorderRadius.circular(17)),
+              child: const Center(
+                  child: Text(
+                'Reset',
+                style: TextStyle(fontFamily: 'Jomolhari', fontSize: 24),
+              )),
+            ),
+          ),
         ),
       ])),
     );
