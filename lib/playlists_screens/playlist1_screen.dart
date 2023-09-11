bd93b6a4e0c996d90c1dfcd568ec35ec604b3aac
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-09-11 14:42:30 +0500

    playscreen designs

--- lib/playlists_screens/playlist1_screen.dart
+++ lib/playlists_screens/playlist1_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:musify_app/songs_screen/Song_screen.dart';
 
 class Playlist1 extends StatefulWidget {
   const Playlist1({super.key});
@@ -13,25 +12,50 @@ class _Playlist1State extends State<Playlist1> {
     "song 1",
     "song 2",
     "song 3",
+    "song 1",
+    "song 2",
+    "song 3",
+    "song 1",
+    "song 2",
+    "song 3",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         elevation: 1,
       ),
       body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: ListView.builder(
           itemCount: names.length,
+          physics:
+              const NeverScrollableScrollPhysics(), // Disable scrolling of inner ListView
           shrinkWrap: true,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(names[index]),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const Song1()));
-              },
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(width: 2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Color(0xffF22355),
+                  child: Icon(
+                    Icons.music_note,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: const Text('By Mesut Kurtis'),
+                tileColor: const Color.fromRGBO(248, 248, 248, 0.092),
+                title: Text(names[index]),
+                trailing: const Icon(Icons.more_vert),
+                onTap: () {},
+              ),
             );
           },
         ),
--- lib/playlists_screens/playlist2_screen.dart
+++ lib/playlists_screens/playlist2_screen.dart
@@ -8,13 +8,57 @@ class Playlist2 extends StatefulWidget {
 }
 
 class _Playlist2State extends State<Playlist2> {
+  List<String> names2 = [
+    "Al Medina",
+    "Tala Al Badru",
+    "Ya Men",
+    "Rehmatullil Alameen",
+    "Balaghal Ula",
+    "Al Hubi",
+    "Assalat U Wassalam",
+    "Medalay",
+    "Maula Ya Salim",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         elevation: 1,
       ),
-      body: const SingleChildScrollView(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: ListView.builder(
+          itemCount: names2.length,
+          physics:
+              const NeverScrollableScrollPhysics(), // Disable scrolling of inner ListView
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(width: 2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Color(0xffF22355),
+                  child: Icon(
+                    Icons.music_note,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: const Text('By Mesut Kurtis'),
+                tileColor: const Color.fromRGBO(248, 248, 248, 0.092),
+                title: Text(names2[index]),
+                trailing: const Icon(Icons.more_vert),
+                onTap: () {},
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
