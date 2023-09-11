92ceb054e25bf998a6042c41929c11fbc8a0de2e
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-09-11 15:05:28 +0500

    songs added but not original

--- lib/playlists_screens/playlist5.dart
+++ lib/playlists_screens/playlist5.dart
@@ -8,11 +8,56 @@ class Playlist5 extends StatefulWidget {
 }
 
 class _Playlist5State extends State<Playlist5> {
+  List<String> names5 = [
+    "song 1",
+    "song 2",
+    "song 3",
+    "song 4",
+    "song 5",
+    "song 6",
+    "song 7",
+    "song 8",
+    "song 9",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         elevation: 1,
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: ListView.builder(
+          itemCount: names5.length,
+          physics:
+              const NeverScrollableScrollPhysics(), // Disable scrolling of inner ListView
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(width: 2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Color(0xffF22355),
+                  child: Icon(
+                    Icons.music_note,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: const Text('By Mesut Kurtis'),
+                tileColor: const Color.fromRGBO(248, 248, 248, 0.092),
+                title: Text(names5[index]),
+                trailing: const Icon(Icons.more_vert),
+                onTap: () {},
+              ),
+            );
+          },
+        ),
       ),
     );
   }
